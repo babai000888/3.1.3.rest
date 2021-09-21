85c927cc5bf4ab04f02510e21955752ab020a19f
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-09-21 22:38:45 +0300

    Initial

--- src/main/java/com/babai/spring_boot/controller/MainController.java
+++ src/main/java/com/babai/spring_boot/controller/MainController.java
@@ -1,26 +1,17 @@
 package com.babai.spring_boot.controller;
 
 import com.babai.spring_boot.model.Role;
-import com.babai.spring_boot.model.User;
 import com.babai.spring_boot.service.RoleService;
-import com.babai.spring_boot.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
-    private final UserService userService;
     private final RoleService roleService;
 
     //Start page
